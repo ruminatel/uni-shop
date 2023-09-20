2416d335359ea789625db694425001e4b58c4b4b
game <rongjdai@163.com>
2023-09-20 20:06:05 +0800

    界面

--- components/my-goods/my-goods.vue
+++ components/my-goods/my-goods.vue
@@ -3,15 +3,20 @@
     <view class="goods-item">
       <!-- 左侧图片区域 -->
       <view class="goods-item-left">
-        <image :src="goods.goods_small_logo || defaultPic" class="goods-pic"></image>
+        <image
+          :src="goods.goods_small_logo || defaultPic"
+          class="goods-pic"
+        ></image>
       </view>
       <!-- 右侧商品详细信息 -->
       <view class="goods-item-right">
         <!-- 商品标题 -->
-        <view class="goods-name">{{goods.goods_name}}</view>
+        <view class="goods-name">{{ goods.goods_name }}</view>
         <!-- 商品价格 -->
         <view class="goods-info-box">
-          <view class="goods-price">￥{{goods.goods_price | tofixed}}</view>
+          <view class="goods-price">￥{{ goods.goods_price | tofixed }}</view>
+          <!-- 商品数量 -->
+          <uni-number-box :min="1"></uni-number-box>
         </view>
       </view>
     </view>
@@ -19,27 +24,28 @@
 </template>
 
 <script>
-  export default {
-    name:"my-goods",
-    data() {
-      return {
-        // 默认的空图片
-        defaultPic: 'https://img3.doubanio.com/f/movie/8dd0c794499fe925ae2ae89ee30cd225750457b4/pics/movie/celebrity-default-medium.png'
-      };
+export default {
+  name: "my-goods",
+  data() {
+    return {
+      // 默认的空图片
+      defaultPic:
+        "https://img3.doubanio.com/f/movie/8dd0c794499fe925ae2ae89ee30cd225750457b4/pics/movie/celebrity-default-medium.png",
+    };
+  },
+  props: {
+    goods: {
+      type: Object,
+      defaul: {},
     },
-    props: {
-      goods: {
-        type: Object,
-        defaul: {}
-      }
+  },
+  filters: {
+    // 把数字处理为带两位小数点的数字
+    tofixed(num) {
+      return Number(num).toFixed(2);
     },
-    filters: {
-      // 把数字处理为带两位小数点的数字
-      tofixed(num) {
-        return Number(num).toFixed(2)
-      }
-    }
-  }
+  },
+};
 </script>
 
 <style lang="scss">
